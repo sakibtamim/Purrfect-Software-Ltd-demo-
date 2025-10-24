bd00192d09a81fadf9fb2ca9152a548616b6faf9
sakibtamim <sakib.tamim@gmail.com>
2025-10-25 00:40:31 +0600

    feat: enhance navbar components; improve layout and styling for better user experience

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,40 +1,63 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Container from "../shared/Container";
 import Image from "next/image";
 import Link from "next/link";
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
   return (
-    <section className="py-[30px]">
+    <section className="lg:py-[30px] py-4  sticky top-0  w-full bg-transparent backdrop-blur-md shadow-md z-50">
       <Container>
-        <div className="flex items-center ">
+        <div className="flex items-center justify-between relative">
           <div className="w-[30%]">
-            {/* <img src="/logo.png" alt="" /> */}
             <Image src="/logo.png" alt="logo" width={48} height={36} />
           </div>
-          <div className="w-[70%] ">
-            <ul className="flex items-center justify-end gap-x-[30px] text-[14px] text-[#979797] font-inter font-semibold">
-              <li>
-                <Link href="/">Services</Link>
-              </li>
-              <li>
-                <Link href="/">Projects</Link>
-              </li>
-              <li>
-                <Link href="/">About us</Link>
-              </li>
-              <li>
-                <Link href="/">Contact us</Link>
-              </li>
-              <li>
-                <Link
-                  href="/"
-                  className="py-[11px] px-[45px]  bg-[#1A0E7D] text-white rounded-[21px] font-[Nexa] font-extrabold"
-                >
+          <div className="lg:w-[70%] w-full lg:static absolute left-0 top-[46px]">
+            <ul
+              className={`w-full lg:flex items-center justify-end gap-x-[30px] lg:text-[14px] text-[18px] text-[#979797] font-inter font-semibold ${
+                show ? "bg-gray-200 text-center " : "lg:mt-0 mt-1000"
+              }`}
+            >
+              <Link
+                href="/"
+                className="block lg:border-none border-b-2 border-b-[#979797] lg:py-0 py-10"
+              >
+                <li>Services</li>
+              </Link>
+              <Link
+                href="/"
+                className="block lg:border-none border-b-2 border-b-[#979797] lg:py-0 py-10"
+              >
+                <li>Projects</li>
+              </Link>
+              <Link
+                href="/"
+                className="block lg:border-none border-b-2 border-b-[#979797] lg:py-0 py-10"
+              >
+                <li>About us</li>
+              </Link>
+              <Link
+                href="/"
+                className="block lg:border-none border-b-2 border-b-[#979797] lg:py-0 py-10"
+              >
+                <li>Contact us</li>
+              </Link>
+              <Link href="/" className="block lg:py-0 py-10">
+                <li className="py-[11px] px-[45px]  bg-[#1A0E7D] text-[16px] text-white rounded-[21px] font-[Nexa] inline-block">
                   Get in touch
-                </Link>
-              </li>
+                </li>
+              </Link>
             </ul>
+          </div>
+          <div className="lg:hidden block" onClick={() => setShow(!show)}>
+            {show ? (
+              <IoClose className="text-3xl text-[#1A0E7D]" />
+            ) : (
+              <IoMenu className="text-3xl text-[#1A0E7D]" />
+            )}
           </div>
         </div>
       </Container>
--- app/components/shared/Container.jsx
+++ app/components/shared/Container.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Container = ({ children }) => {
-  return <div className="max-w-7xl mx-auto">{children}</div>;
+  return <div className="max-w-7xl mx-auto lg:px-0 px-1">{children}</div>;
 };
 
 export default Container;
